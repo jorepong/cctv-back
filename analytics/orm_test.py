b45dbbe6a26a7c79fc29b3d4874c12e16ad87edf
jorepong <jorepong123@naver.com>
2025-05-20 14:26:11 +0900

    orm_test.py 코드 수정

--- analytics/orm_test.py
+++ analytics/orm_test.py
@@ -6,13 +6,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmartCCTV.settings')
 django.setup()
 
 from SmartCCTV.settings import start_ssh_tunnel
-
 start_ssh_tunnel()
 
 from analytics.models import Cameras
 
-# print(Cameras.objects.first())
-
 cams = Cameras.objects.all()
 print(cams.count())
-# print(cams.first().camera_id)
